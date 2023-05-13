f8140a844797a7467b82bd323ceb58033f28ee33
omarordeche <omarordeche1997@gmail.com>
2023-05-13 11:13:51 +0100

    'Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { BrowserRouter , Routes ,Route} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path='/About' element={<About />} />
+    <Route path="*" element={<NotFound />} /> 
+     {/* ou bien utiliser cette route pour dire que cette page n'éxiste pas  */}
+    {/* <Route path='*' element={<Home />} /> */}
+       
+   </Routes>
+   </BrowserRouter>
+   </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+<img src='./world.png' alt="logo react" />
+<h3>COUNTRIES</h3>            
+        </div>
+    );
+};
+
+export default Logo;
